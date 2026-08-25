da98c2d575f2d5aba89791665178e7fcfaf42312
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-25 16:06:10 +0200

    Refresh Harmony owner for v1.7.6

--- DSFix/SubModule.cs
+++ DSFix/SubModule.cs
@@ -9,7 +9,7 @@ namespace DSFix
 {
     public sealed class SubModule : MBSubModuleBase
     {
-        internal const string HarmonyId = "xmarre.dsfix.bannerlord.1.3.15.tor.witm.1.16.distinguishedservice.7.5";
+        internal const string HarmonyId = "xmarre.dsfix.bannerlord.1.3.15.tor.witm.1.16.distinguishedservice.7.6";
         private Harmony _harmony;
         private AssemblyLoadEventHandler _assemblyLoadHandler;
 
